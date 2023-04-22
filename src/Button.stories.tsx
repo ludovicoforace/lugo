7a948524ab37e6531c28fdb1c6a1208abb5667d0
Ludovico Forace <ludovicoforace@yahoo.com>
2023-04-22 23:23:55 +0100

    Remove unnecessarely files

---
+++ src/Button.stories.tsx
@@ -0,0 +1,10 @@
+import Button from './Button'
+
+export default {
+  title: 'Design System/Button',
+  component: Button,
+}
+
+export const RegularButton = () => <Button>Click me</Button>
+
+RegularButton.storyName = 'regular button'
