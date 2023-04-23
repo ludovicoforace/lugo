f2dd8afb4e2cda6aec91c0c98f56d39c9cae344a
Ludovico Forace <ludovicoforace@yahoo.com>
2023-04-24 00:48:31 +0100

    Configure build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'Lugo',
+      fileName: (format) => `lugo.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    sourcemap: true,
+  },
 })
