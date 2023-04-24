d971a949bf34b4de228827c33074e2614fdc12d4
Ludovico Forace <ludovicoforace@yahoo.com>
2023-04-24 19:27:59 +0100

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -14,13 +14,11 @@ export default defineConfig({
       fileName: (format) => `lugo.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'styled-components', 'polished'],
+      external: ['react', 'react-dom'],
       output: {
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
-          'styled-components': 'StyledComponents',
-          polished: 'Polished',
         },
       },
     },
