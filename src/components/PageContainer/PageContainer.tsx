bd640b4f1bca30a4efafd8a92017daea2937126a
Ludovico Forace <ludovicoforace@yahoo.com>
2023-04-28 00:27:30 +0100

    Adjust container

--- src/components/PageContainer/PageContainer.tsx
+++ src/components/PageContainer/PageContainer.tsx
@@ -1,16 +1,12 @@
 import styled from 'styled-components'
 import { spacing } from '../../shared/styles'
 
-const Container = styled.div<{
-  maxWidth: string
-  sideMargin: string
-}>`
+const Container = styled.div<{ maxWidth: string }>`
   ${(props) => `
     margin: 0 auto;
     max-width: ${props.maxWidth};
 
-    @media screen and (max-width: calc(${props.maxWidth} + ${spacing.maxScrollbarWidth} + ${props.sideMargin} * 2)) {
-      margin: 0 ${props.sideMargin};
+    @media screen and (max-width: calc(${props.maxWidth} + ${spacing.maxScrollbarWidth})) {
       max-width: none;
     }
   `}
